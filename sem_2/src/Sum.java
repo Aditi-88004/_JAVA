7c4d6ee7dfb2d729370b1cce1195ccc17eeeaa61
Aditi <Aditi@DESKTOP-4SMV0M3>
2021-05-11 22:01:14 +0530

    Program 7

---
+++ sem_2/src/Sum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Sum {
+
+	public static void main(String[] args) {
+		int num,sum=0,c;
+	      Scanner scan =new Scanner(System.in);
+	      do{
+	         System.out.println("Enter the number: ");
+	         num=scan.nextInt();
+	         sum+=num;
+	         System.out.println("The sum of numbers is: "+sum);
+	         System.out.println("do you want to continue this program type 1");  
+	         c=scan.nextInt();
+	      }while(c==1);
+
+	}
+
+}
