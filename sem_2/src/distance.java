70fc53b2eab5cc1d6bd1143dab83838f2c5e1044
Aditi <Aditi@DESKTOP-4SMV0M3>
2021-05-25 21:10:29 +0530

    Program 8

--- sem_2/src/distance.java
+++ sem_2/src/distance.java
@@ -1,7 +1,24 @@
 
 public class distance {
-
+    int feet,inches;
+    distance(int feet, int inches){
+    	this.feet = feet;
+    	this.inches = inches;
+    }
+    public static distance cal(distance d1, distance d2) {
+    	distance d=new distance(0,0);
+      	d.inches = d2.inches - d1.inches;
+    	d.feet = d2.feet - d1.feet  + (d.inches/12);
+    	d.inches = d.inches%12;
+    	return d;
+    }
 	public static void main(String[] args) {
+		distance d1 = new distance(20,10);
+		distance d2 = d1;
+		distance d = cal(d1,d2);
+		System.out.println("point 1: "+d1.feet+" feet "+d1.inches+" inches");
+		System.out.println("point 2: "+d2);
+		System.out.println("Distance: "+d);
 		
 	}
 
